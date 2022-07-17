01f9c10c3b989bfef2dfc9b036d057eae86fe362
Chanhee Jeong <chan420420@gmail.com>
2022-07-18 02:18:27 +0900

    ✨ feat : UIButton Extension 추가

---
+++ Inception/Inception/Global/Extension/View/UIButton+.swift
@@ -0,0 +1,28 @@
+//
+//  UIButton+.swift
+//  Inception
+//
+//  Created by Chanhee Jeong on 2022/07/17.
+//
+//  ref: https://ios-development.tistory.com/742
+
+import UIKit
+
+extension UIButton {
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+    
+    func setImageTintColor(_ color: UIColor) {
+        let tintedImage = self.imageView?.image?.withRenderingMode(.alwaysTemplate)
+        self.setImage(tintedImage, for: .normal)
+        self.tintColor = color
+    }
+    
+}
--- Inception/Inception/Global/Extension/View/UIView+.swift
+++ Inception/Inception/Global/Extension/View/UIView+.swift
@@ -5,7 +5,6 @@
 //  Created by Chanhee Jeong on 2022/07/17.
 //
 
-import Foundation
 import UIKit
 
 extension UIView {
