7a480cac733f5c6af6bb46227606018cef71ff38
compuTasha <mijookim.tasha@gmail.com>
2022-07-17 17:37:45 +0900

    ✅ add : 상단 네비게이션 타이틀 추가 #6

---
+++ Inception/Inception/Screens/AlarmList/AlarmListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AlarmListViewController.swift
+//  Inception
+//
+//  Created by Mijoo Kim on 2022/07/17.
+//
+
+import Foundation
+import UIKit
+
+class AlarmListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
