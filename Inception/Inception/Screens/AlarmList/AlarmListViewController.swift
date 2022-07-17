a5c047a25b2681327c011145af97ad71e5b9f991
compuTasha <mijookim.tasha@gmail.com>
2022-07-17 20:55:01 +0900

    ✅ add : 라벨, 버튼, 테이블 레이아웃 배치 #6

--- Inception/Inception/Screens/AlarmList/AlarmListViewController.swift
+++ Inception/Inception/Screens/AlarmList/AlarmListViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AlarmListViewController: UIViewController {
 
+    @IBOutlet weak var clearPresentAlarmButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
