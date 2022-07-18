2cc6d8d2cf31df9bf5ca3c1bc9a09e829bb86a10
compuTasha <mijookim.tasha@gmail.com>
2022-07-18 11:25:37 +0900

    ✨ feat : 테이블 delegate, data source 연결 #6

--- Inception/Inception/Screens/AlarmList/AlarmListViewController.swift
+++ Inception/Inception/Screens/AlarmList/AlarmListViewController.swift
@@ -8,14 +8,65 @@
 import Foundation
 import UIKit
 
-class AlarmListViewController: UIViewController {
+class AlarmListViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+
+    
 
     @IBOutlet weak var clearPresentAlarmButton: UIButton!
+    @IBOutlet weak var presentTableView: UITableView!
+    @IBOutlet weak var savedTableView: UITableView!
+    
+    let presentAlarms = ["09:00"]
+    let savedAlarms = ["10:00", "11:00", "12:00"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        presentTableView.delegate = self
+        presentTableView.dataSource = self
+        presentTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        savedTableView.delegate = self
+        savedTableView.dataSource = self
+        savedTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if tableView == presentTableView {
+            return presentAlarms.count
+        }
+        else if tableView == savedTableView {
+            return savedAlarms.count
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        print(indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        
+        if tableView == self.presentTableView {
+            if presentAlarms.count > 0 {
+                content.text = savedAlarms[indexPath.row]
+                print("function call")
+                cell.contentConfiguration = content
+                return cell
+            }
+        }
+        else if tableView == self.savedTableView {
+            if savedAlarms.count > 0 {
+                content.text = savedAlarms[indexPath.row]
+                cell.contentConfiguration = content
+                return cell
+            }
+        }
+        return cell
+    }
+    
 }
