cc669b38927b2eb42b1d15581f7f7acfc7a626f3
Taeho Kim <xogh8025@gmail.com>
2022-07-19 11:30:26 +0900

    ✅ add : 그라데이션 원 구현

--- Inception/Inception/Screens/AlarmClock/AlarmClockViewController.swift
+++ Inception/Inception/Screens/AlarmClock/AlarmClockViewController.swift
@@ -12,37 +12,63 @@ class AlarmClockViewController: UIViewController {
   @IBOutlet weak var morningCircle: UIView!
   @IBOutlet weak var ampmLabel: UILabel!
   @IBOutlet weak var timeLabel: UILabel!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    setTime()
-    
-    Timer.scheduledTimer(withTimeInterval: 1,  repeats: true){ (_) in
-      self.setTime()
-    }
     
     titleLabel.numberOfLines = 0
     titleLabel.text = "개운한 아침을 위해" + "\n지금 기상하세요"
     
-    morningCircle.layer.masksToBounds = true
-    morningCircle.layer.cornerRadius = 135
-    morningCircle.layer.borderWidth = 3
-    morningCircle.layer.borderColor = UIColor.orange.cgColor
+    // MARK: 시간 설정
+    
+    setTime()
+    Timer.scheduledTimer(withTimeInterval: 1,  repeats: true){ (_) in
+      self.setTime()
+    }
+    
+    // MARK: 그라데이션 원 그리기
+    
+    drawGradientCircle()
   }
   
+
   private func setTime(){
     let date = Date()
-    
+
     let dateFormatter = DateFormatter()
     let ampmFormatter = DateFormatter()
-    
+
     dateFormatter.dateFormat = "hh: mm"
     ampmFormatter.dateFormat = "a"
-    
+
     let currentTime = dateFormatter.string(from: date)
     let ampmSetter = ampmFormatter.string(from: date)
-    
+
     self.timeLabel.text = currentTime
     self.ampmLabel.text = ampmSetter
-  }// setTime
+  }
+  
+  private func drawGradientCircle() {
+    let lineWidth: CGFloat = 3
+    let originOffset: CGFloat = lineWidth
+    let circleDiameter: CGFloat = 278
+
+    let gradient = CAGradientLayer()
+
+    gradient.frame =  CGRect(origin: CGPoint(x: 0, y:  0),
+                             size: morningCircle.frame.size)
+    gradient.colors = [UIColor.white.cgColor, UIColor.systemOrange.cgColor]
+
+    let rectangle = CGRect(origin: CGPoint(x: originOffset, y: originOffset),
+                           size: CGSize(width: circleDiameter - lineWidth*2,
+                                        height: circleDiameter - lineWidth*2))
+
+    let shape = CAShapeLayer()
+    shape.lineWidth = lineWidth
+    shape.path = UIBezierPath(ovalIn: rectangle).cgPath
+    shape.strokeColor = UIColor.black.cgColor
+    shape.fillColor = UIColor.clear.cgColor
+    gradient.mask = shape
+    morningCircle.layer.addSublayer(gradient)
+  }
 }
