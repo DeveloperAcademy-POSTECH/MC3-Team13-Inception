a2c5bf8de94fba402e994da3bb67645f81b7214b
neoul <ebkang1223@gmail.com>
2022-07-19 12:33:52 +0900

    ✅add: 코드 스타일 수정 #1

--- Inception/Inception/AlarmCalculator/AlarmCalculatorViewController.swift
+++ Inception/Inception/AlarmCalculator/AlarmCalculatorViewController.swift
@@ -10,31 +10,21 @@
 import UIKit
 
 class AlarmCalculatorViewController: UIViewController {
-    
-        
-    
-    
-    @IBOutlet weak var firstView: UIView!
-    @IBOutlet weak var secondView: UIView!
-    
-    
-        
-        @IBAction func switchViews(_ sender: UISegmentedControl)
-        {
-            if sender.selectedSegmentIndex == 0 {
-                firstView.alpha = 0
-                secondView.alpha = 1
-            }   else {
-                firstView.alpha = 1
-                secondView.alpha = 0
-            }
-        }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        navigationItem.largeTitleDisplayMode = .never
-        
-        
-        // Do any additional setup after loading the view.
-    }
-}
+  @IBOutlet weak var firstView: UIView!
+  @IBOutlet weak var secondView: UIView!
 
+  //세그먼트 컨트롤 뷰를 변경시킴
+  @IBAction func switchViews(_ sender: UISegmentedControl) {
+    if sender.selectedSegmentIndex == 0 {
+      firstView.alpha = 0
+      secondView.alpha = 1
+    }   else {
+      firstView.alpha = 1
+      secondView.alpha = 0
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
