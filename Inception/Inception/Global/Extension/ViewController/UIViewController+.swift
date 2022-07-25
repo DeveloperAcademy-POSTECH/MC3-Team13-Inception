f34941d67bbaf0b3ed7db35240154ee1365f772a
Chanhee Jeong <chan420420@gmail.com>
2022-07-25 20:32:20 +0900

    ✨feat : 탭바 추가 및 화면연결

---
+++ Inception/Inception/Global/Extension/ViewController/UIViewController+.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+.swift
+//  Inception
+//
+//  Created by Chanhee Jeong on 2022/07/25.
+//
+
+import UIKit
+
+extension UIViewController {
+  static func instantiateViewController<ViewController: UIViewController>() -> ViewController {
+    let identifier: String = String(describing: self)
+    
+    let storyboard = UIStoryboard(name: identifier, bundle: nil)
+    guard let viewController = storyboard.instantiateViewController(withIdentifier: identifier) as? ViewController else { fatalError() }
+    
+    return viewController
+  }
+}
