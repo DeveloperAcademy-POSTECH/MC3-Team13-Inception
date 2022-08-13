26c6104d3e0edbcd50cf3fe4123c52858e537ec6
compuTasha <mijookim.tasha@gmail.com>
2022-08-14 08:14:09 +0900

    ✅ add : 개인정보처리방침 페이지 추가 #73

---
+++ Inception/Inception/Screens/Information/Views/PrivacyPolicyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PrivacyPolicyViewController.swift
+//  Inception
+//
+//  Created by Mijoo Kim on 2022/08/13.
+//
+
+import UIKit
+
+final class PrivacyPolicyViewController: UIViewController, Storyboarded {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
