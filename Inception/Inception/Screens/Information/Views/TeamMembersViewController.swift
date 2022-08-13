e0f5b2fb9b8bf2d6f17e1f78af06cf214eaa6041
compuTasha <mijookim.tasha@gmail.com>
2022-08-13 21:31:14 +0900

    ✅ add : 팀멤버 깃허브 웹사이트 연결 #73

--- Inception/Inception/Screens/Information/Views/TeamMembersViewController.swift
+++ Inception/Inception/Screens/Information/Views/TeamMembersViewController.swift
@@ -9,10 +9,44 @@ import UIKit
 import SafariServices
 
 final class TeamMembersViewController: UIViewController, Storyboarded {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
   }
   
+  @IBAction func linkToAlolaGit(_ sender: Any) {
+    let githubURL = NSURL(string: "https://github.com/compuTasha")
+    let githubSafariView: SFSafariViewController = SFSafariViewController(url: githubURL! as URL)
+    self.present(githubSafariView, animated: true, completion: nil)
+  }
+  
+  @IBAction func linkToAveryGit(_ sender: Any) {
+    let githubURL = NSURL(string: "https://github.com/chaneeii")
+    let githubSafariView: SFSafariViewController = SFSafariViewController(url: githubURL! as URL)
+    self.present(githubSafariView, animated: true, completion: nil)
+  }
+  
+  @IBAction func linkToMilliGit(_ sender: Any) {
+    let githubURL = NSURL(string: "https://github.com/jinccc97")
+    let githubSafariView: SFSafariViewController = SFSafariViewController(url: githubURL! as URL)
+    self.present(githubSafariView, animated: true, completion: nil)
+  }
+  
+  @IBAction func linkToNeoulGit(_ sender: Any) {
+    let githubURL = NSURL(string: "https://github.com/pppaper")
+    let githubSafariView: SFSafariViewController = SFSafariViewController(url: githubURL! as URL)
+    self.present(githubSafariView, animated: true, completion: nil)
+  }
+  
+  @IBAction func linkToNickGit(_ sender: Any) {
+    let githubURL = NSURL(string: "https://github.com/tea-hkim")
+    let githubSafariView: SFSafariViewController = SFSafariViewController(url: githubURL! as URL)
+    self.present(githubSafariView, animated: true, completion: nil)
+  }
+  
+  @IBAction func linkToTannyGit(_ sender: Any) {
+    let githubURL = NSURL(string: "https://github.com/taehyeonk")
+    let githubSafariView: SFSafariViewController = SFSafariViewController(url: githubURL! as URL)
+    self.present(githubSafariView, animated: true, completion: nil)
+  }
 }
