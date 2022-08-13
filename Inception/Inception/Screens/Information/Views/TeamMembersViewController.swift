0292565c51d604e55f8f18bd7738a2cf32726ae2
compuTasha <mijookim.tasha@gmail.com>
2022-08-13 18:47:00 +0900

    ✅ add : 팀멤버 페이지 생성 #73

---
+++ Inception/Inception/Screens/Information/Views/TeamMembersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TeamMembersViewController.swift
+//  Inception
+//
+//  Created by Mijoo Kim on 2022/08/12.
+//
+
+import UIKit
+import SafariServices
+
+final class TeamMembersViewController: UIViewController, Storyboarded {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+  
+}
